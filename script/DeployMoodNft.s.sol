a7fbde91467c942f7f490a4994993584aa654570
Aakarshit Agarwal <aakarshit.agarwal@gmail.com>
2023-08-04 22:45:51 +0530

    Complete DeployMoodNft script

--- script/DeployMoodNft.s.sol
+++ script/DeployMoodNft.s.sol
@@ -2,16 +2,21 @@
 
 pragma solidity ^0.8.20;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {MoodNft} from "../src/MoodNft.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract DeployMoodNft is Script {
     function run() external returns (MoodNft) {
+        string memory happySvg = vm.readFile("./img/happy.svg");
+        string memory sadSvg = vm.readFile("./img/sad.svg");
         vm.startBroadcast();
-        // MoodNft moodNft = new MoodNft();
+        MoodNft moodNft = new MoodNft(
+            svgToImageUri(happySvg),
+            svgToImageUri(sadSvg)
+        );
         vm.stopBroadcast();
-        // return moodNft;
+        return moodNft;
     }
 
     function svgToImageUri(
